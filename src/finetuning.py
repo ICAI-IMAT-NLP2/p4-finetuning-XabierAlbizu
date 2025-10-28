799dfa3c8cc0dfcda20faf8f0524c79ece880db2
XabierAlbizu <202207372@alu.comillas.edu>
2025-10-28 12:47:36 +0100

    pt finished

--- src/finetuning.py
+++ src/finetuning.py
@@ -8,7 +8,7 @@ except:
     from src.utils import download_and_load_model
 
 class LoRA(nn.Module):
-    def __init__(self, original_layer, r=4, alpha=32):
+    def __init__(self, original_layer: nn.Linear, r=4, alpha=32):
         """
         Low-Rank Adaptation (LoRA) module.
         
@@ -19,29 +19,35 @@ class LoRA(nn.Module):
         """
         super().__init__()
         # TODO: Initialize LoRA parameters
-        self.r = None
-        self.alpha = None
-        self.original_layer = None
+        self.r = r
+        self.alpha = alpha
+        self.original_layer = original_layer
+
+        D, K = self.original_layer.in_features, self.original_layer.out_features
 
         # TODO: Low-rank matrices A and B for LoRA
-        self.A = None
-        self.B = None
+        self.A = nn.Parameter(torch.empty(D, self.r))
+        self.B = nn.Parameter(torch.zeros(self.r, K))
 
         # TODO: Initialize LoRA weights (B is zero-initialized, A is random)
-        nn.init.kaiming_uniform_(None)
+        nn.init.kaiming_uniform_(self.A, a=math.sqrt(5))
         
         # TODO: Scaling factor alpha 
-        self.scaling = None
+        self.scaling = self.alpha / self.r
 
         # TODO: Freeze the original layer parameters
-        for param in None:
+        for param in self.original_layer.parameters():
             param.requires_grad = False
                 
     def forward(self, x):
         # TODO: Perform forward pass with low-rank update
-        return None
+        out = self.original_layer(x)
+        out_lora = (x @ self.A) @ self.B
+        out = out + self.scaling * out_lora
 
-def inject_lora_into_model(model, r=4, alpha=32, device='cpu'):
+        return out
+
+def inject_lora_into_model(model:nn.Module, r=4, alpha=32, device='cpu'):
     """
     Inject LoRA layers into the linear layers of the attention modules of the model.
     
@@ -55,15 +61,15 @@ def inject_lora_into_model(model, r=4, alpha=32, device='cpu'):
         model (PreTrainedModel): The model with LoRA injected into attention layers.
     """
     # TODO: Iterate through all child modules of the model
-    for child_name, child_module in None:
+    for child_name, child_module in model.named_children():
         # TODO: Check if the child module is a linear layer of the attention module
-        if child_name.lower() in None:
+        if child_name.lower() in ["q", "o", "k", "v"]:
             # TODO: Create LoRA layer for linear module
-            lora_layer = None
+            lora_layer = LoRA(child_module, r, alpha)
             setattr(model, child_name, lora_layer)
         else:
             # TODO: Recursively inject LoRA into child module
-            pass
+            inject_lora_into_model(child_module, r, alpha, device)
     return model.to(device)
 
 
@@ -78,7 +84,7 @@ class SoftPromptEmbedding(nn.Module):
         """
         super().__init__()
         # TODO: Initialize soft prompt embeddings
-        self.soft_prompt = None
+        self.soft_prompt = nn.Parameter(torch.empty(prompt_length, model_hidden_size))
 
     def forward(self, input_embeddings):
         """
@@ -91,8 +97,13 @@ class SoftPromptEmbedding(nn.Module):
             torch.Tensor: The concatenated soft prompts and original embeddings.
         """
         # TODO: Expand soft prompt to match batch size
-        batch_size = None
-        soft_prompt_expanded = None
+        batch_size = input_embeddings.shape[0]
+        soft_prompt_expanded = self.soft_prompt.unsqueeze(0).repeat(batch_size,1,1)
+
+        concatenated = torch.cat([
+            soft_prompt_expanded,
+            input_embeddings
+        ], dim=1)
 
         # TODO: Concatenate soft prompt and input embeddings
-        return None
+        return concatenated
